ffb879969b305a9d6ff0bda0697ce4ffd8da5f75
Eric Theuri <tericmail001@gmail.com>
2025-08-03 18:58:16 +0300

    Modified the backend url in chatbot.jsx

--- client/src/components/Chatbot.jsx
+++ client/src/components/Chatbot.jsx
@@ -36,7 +36,7 @@ const Chatbot = () => {
     setIsTyping(true)
 
     try {
-      const response = await fetch("http://localhost:8000/api/generate-blog", {
+      const response = await fetch("https://mindtype.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
