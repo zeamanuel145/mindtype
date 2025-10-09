14a831406aa5cdd70d11afc6b908d5738f372360
Eric Theuri <tericmail001@gmail.com>
2025-10-09 22:14:44 +0300

    Modified frontend to allow all origins

--- src/social_media_blog/app.py
+++ src/social_media_blog/app.py
@@ -49,11 +49,12 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:8000",
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
