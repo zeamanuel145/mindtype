61c09431ce6fae29f7d52ed7d3574cfec0ab14c4
Eric Theuri <tericmail001@gmail.com>
2025-10-09 12:53:12 +0300

    Changed the endpoint from localhost to the deployed blackend

--- client/src/components/ChatBot.jsx
+++ client/src/components/ChatBot.jsx
@@ -36,7 +36,7 @@ const Chatbot = () => {
     setIsTyping(true)
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("https://mindtype.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
